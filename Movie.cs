5468c82d87009930373427d35a5c51ca52666fa6
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-03-25 20:12:52 -0500

    first commit

---
+++ Movie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpExercises_28
+{
+    class Movie
+    {
+
+        public string title;
+        public string director;
+        // to avoid giving access to the rating we make it private in the movie class
+        private string rating;
+        // only code within the Movie class can access the rating if a "private" modifier is used
+
+
+        // this is a contructor where we pass in the title, director, and rating
+        // a parameters and the values are assinged to each object 
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;
+        }
+
+        // define getters and setters 
+        public string Rating
+        {
+            get { return rating; }
+            set
+                // we set up a rule to test if things are equal to the allowed ratings
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpExercises_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // getters and setters help to make you data more secure
+            // also set rules for types of data that are allowed 
+
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+
+            // G, PG, PG-13, R, NR 
+            avengers.Rating = "Dog Woof";  // results in NR default rating when setter verifies input
+            //avengers.Rating = "R";
+
+            // shrek.Rating = "Cat";
+            // shrek.Rating = "R";
+
+            // to avoid giving access to the rating we make it private in the movie class
+            Console.WriteLine(avengers.Rating);
+            Console.WriteLine(shrek.Rating);
+            Console.ReadLine();
+        }
+    }
+}
